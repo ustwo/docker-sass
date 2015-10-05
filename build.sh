05e9b761a59986d9578dc83968938b6d7eed25ad
Arnau Siches <arnau@ustwo.com>
2015-10-05 10:52:10 +0100

    Optimise image size to ~9.5MB

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ SASSC_VERSION=3.2.5
 LIBSASS_VERSION=3.2.5
 SASS_LIBSASS_PATH=/usr/local/libsass-$LIBSASS_VERSION
 
-apk add --update \
-      curl \
-      g++ \
-      make
+build_deps="curl g++ make"
+run_deps="libstdc++"
+
+apk add --update $build_deps
 rm -rf /var/cache/apk/*
 
 cd /usr/local
@@ -20,11 +20,12 @@ curl -L https://github.com/sass/sassc/archive/$SASSC_VERSION.tar.gz | tar xvz
 
 # Compile
 cd /usr/local/sassc-$SASSC_VERSION
-make
+make BUILD=static
 
 # Clean
 cp /usr/local/sassc-$SASSC_VERSION/bin/sassc /usr/local/bin/sassc
 rm -rf /usr/local/sassc-$SASSC_VERSION \
        /usr/local/libsass-$LIBSASS_VERSION
 
-apk del curl make
+apk del $build_deps
+apk add --update $run_deps
